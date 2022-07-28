75b1f73ad3cce481dc4fd42067be32923ddc85f1
AJ Williams <a.james.williams1@gmail.com>
2022-07-28 15:02:56 -0700

    Add untested arcade drive

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -6,7 +6,7 @@ using namespace pros;
 void opcontrol()
 {
     /* SETUP DEVICES */
-    Controller master(E_CONTROLLER_MASTER);
+    Controller main(E_CONTROLLER_MASTER);
     Motor fly1(13);
     Motor fly2(14);
 
@@ -17,45 +17,45 @@ void opcontrol()
 
     Motor intake(INTAKE_MOTOR);
 
-    /* Diagnostic Variables */
-    double real_vel = 0;
-    int32_t real_voltage = 0;
-    int32_t real_current = 0;
-    double torque_vel_power = 0;
-    double amps_volts_power = 0;
-    double real_temp = 0;
-    double real_torque = 0;
+    // Joystick variables
+    int32_t joy_l_x = 0;
+    int32_t joy_l_y = 0;
+    int32_t joy_r_x = 0;
+    int32_t joy_r_y = 0;
 
-    /* Print headers for daignostic variables */
-    printf("time (ms),velocity (RPM),voltage (mV),current (mA),torque*vel power (W),current*volts power (W),"
-           "temperature (deg C),torque (Nm)\n");
-
-    fly1.move(-127);
-    fly2.move(-127);
-    // 7 + 7/16 in compression
-
-    /* Time variables */
-    int start_time = millis();
-    double elapsed_time = 0;
+    int32_t l_drive_speed = 0;
+    int32_t r_drive_speed = 0;
 
     while (true)
     {
-        /* Time */
-        elapsed_time = (millis() - start_time) / 1000.0;
-
-        /* Motor stats */
-        real_vel = fly1.get_actual_velocity();
-        real_voltage = fly1.get_voltage();
-        real_current = fly1.get_current_draw();
-        torque_vel_power = fly1.get_power();
-        amps_volts_power = (real_voltage * real_current) / 1000000.0;
-        real_temp = fly1.get_temperature();
-        real_torque = fly1.get_torque();
-
-        printf("%.2f, %.3f, %i, %i, %.3f, %.3f, %.3f, %.3f\n", elapsed_time, real_vel, real_voltage, real_current,
-               torque_vel_power, amps_volts_power, real_temp, real_torque);
-
         /* Actual stuff */
-        delay(50);
+
+        // Intake
+        if (main.get_digital(E_CONTROLLER_DIGITAL_A))
+            intake.move(50);
+
+        /* Drive */
+        joy_l_x = main.get_analog(E_CONTROLLER_ANALOG_LEFT_X);
+        joy_l_y = main.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
+        joy_r_x = main.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
+        joy_r_y = main.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y);
+
+        // Cap the value
+        if (joy_l_y + joy_r_x > 127)
+            l_drive_speed = 127;
+        if (joy_l_y + joy_r_x < -127)
+            l_drive_speed = -127;
+        if (joy_l_y - joy_r_x > 127)
+            r_drive_speed = 127;
+        if (joy_l_y - joy_r_x < -127)
+            r_drive_speed = -127;
+
+        // Move motors
+        drive_fr.move(r_drive_speed);
+        drive_br.move(r_drive_speed);
+        drive_fl.move(l_drive_speed);
+        drive_bl.move(l_drive_speed);
+
+        delay(20);
     }
 }
