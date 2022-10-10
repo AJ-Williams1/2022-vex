c1c469759f3ab241eec158ed36a008a763866d51
AJ Williams <a.james.williams1@gmail.com>
2022-10-10 02:07:11 -0700

    Remove trailing comma in description

--- ports-to-description.sh
+++ ports-to-description.sh
@@ -1,4 +1,4 @@
-#       ports file       | remove comments | del blanks  | no #define          | join lines   | comma-separate  | remove PORT suffix
-ports="$(cat src/ports.h | sed '/\/\/.*/d' | sed '/^$/d' | awk '{print $2,$3}' | tr '\n' '\\' | sed 's/\\/, /g' | sed 's/_PORT//g')"
+#       ports file       | remove comments | del blanks  | no #define          | join lines   | comma-separate  | remove "PORT"    | no trailing ,
+ports="$(cat src/ports.h | sed '/\/\/.*/d' | sed '/^$/d' | awk '{print $2,$3}' | tr '\n' '\\' | sed 's/\\/, /g' | sed 's/_PORT//g' | sed 's/, $//')"
 
 perl -0777 -i -pe "s/.*\"description\".*/      \"description\": \"${ports}\",/" project.pros
