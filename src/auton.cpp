0a37f4fe193b0f745bd160248574a57901679ab0
AJ Williams <a.james.williams1@gmail.com>
2022-10-14 02:19:37 -0700

    Circle driving auton

--- src/auton.cpp
+++ src/auton.cpp
@@ -1,9 +1,28 @@
 #include "main.h"
 #include "ports.h"
-
-#define WHEEL_DIAMETER 4.0
+#include "Drivetrain_main.h"
 
 using namespace pros;
+
+// Constants
+#define WHEEL_DIAMETER 4.0
+#define WHEELBASE_WIDTH 11.5
+#define WHEEL_TO_EDGE 2
+
+// Types
+typedef enum circle_radius_type_e {
+    INNER = -1,
+    OUTER = 1,
+} circle_radius_type_e;
+
+typedef enum circle_direction_e {
+    CLOCKWISE = 1,
+    COUNTERCLOCKWISE = -1,
+} circle_direction_e;
+
+// Function declarations
+void drive_dist(float distance, float vel);
+void drive_circle(circle_radius_type_e radius_type, float radius, float outer_speed, float radians, circle_direction_e direction);
 
 /**
  * Runs the user autonomous code. This function will be started in its own task
@@ -17,7 +36,58 @@ using namespace pros;
  * from where it left off.
  */
 
-void autonomous()
+void auton()
 {
-    //    drive_dist(12, 50);
+    drive_circle(INNER, 24, 80, 360, CLOCKWISE);
+}
+
+// Helper functions
+void drive_dist(float distance, float vel)
+{
+    Drivetrain drive;
+
+    float rotations = distance / (WHEEL_DIAMETER * M_PI);
+    drive.move_relative(rotations, vel);
+}
+
+void drive_circle(circle_radius_type_e radius_type, float radius, float outer_speed, float degrees, circle_direction_e direction)
+{
+    Drivetrain drive;
+
+    // Get both actual radii, with respect to wheels, not edge of robot
+    float inner_radius = 0;
+    float outer_radius = 0;
+
+    switch (radius_type)
+    {
+    case INNER:
+        inner_radius = radius + WHEEL_TO_EDGE;
+        outer_radius = inner_radius + WHEELBASE_WIDTH;
+
+    case OUTER:
+        outer_radius = radius - WHEEL_TO_EDGE;
+        inner_radius = outer_radius - WHEELBASE_WIDTH;
+    }
+
+    // Find the velocities, based on the formula for driving in circles:
+    // (V_outer / V_inner) = (R_outer / R_inner)
+    float inner_speed = outer_speed * (inner_radius / outer_radius);
+
+    // Convert degrees to radians and get arc length
+    float inner_arc_length = (degrees * M_PI / 180) * inner_radius;
+    float outer_arc_length = (degrees * M_PI / 180) * outer_radius;
+
+    float inner_rotations = inner_arc_length / (WHEEL_DIAMETER * M_PI);
+    float outer_rotations = outer_arc_length / (WHEEL_DIAMETER * M_PI);
+
+    switch(direction)
+    {
+        case CLOCKWISE:
+            drive.right.move_relative(inner_rotations, inner_speed);
+            drive.left.move_relative(outer_rotations, outer_speed);
+
+        case COUNTERCLOCKWISE:
+            drive.left.move_relative(inner_rotations, inner_speed);
+            drive.right.move_relative(outer_rotations, outer_speed);
+    }
 }
