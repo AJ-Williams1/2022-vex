c293e8ba91cf9624b2ee316bcda70797311c345a
Mikeyoof25 <70543114+Mikeyoof25@users.noreply.github.com>
2022-11-28 17:02:52 -0800

    Revert "Adjusted P"
    This reverts commit 137c3fe1b9bfe5080a22f3bc7e7c535454cadf7e.

--- src/flywheel.cpp
+++ src/flywheel.cpp
@@ -46,8 +46,7 @@ void flywheel_on_fn()
     int fly2_voltage = 0;
     int ticks = 0;
 
-    // float kP = 0.0018;
-    float kP = 0.06;
+    float kP = 0.0018;
 
     // printf("ticks, fly1_vel, fly2_vel\n");
 
