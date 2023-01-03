6bcdb04b849626bfc4e5ebf36ffbb68e46863ad0
Mikeyoof25 <70543114+Mikeyoof25@users.noreply.github.com>
2023-01-03 12:20:11 -0800

    Changed burned brain ports

--- src/auton.cpp
+++ src/auton.cpp
@@ -1,6 +1,7 @@
 #include "autoSelect/selection.h"
 #include "main.h"
 #include "ports.h"
+#include "pros/rtos.hpp"
 #include "setup_macros.h"
 
 using namespace okapi;
@@ -40,10 +41,8 @@ void tripleAuto(){       // shoot 3 in auton
 
 void doubleAuto(){       // shoot 2 in auton
   for (int i = 0; i < 2; i++){
-    while(!canFire()){
-     
-    } 
-    if(canFire()) index_disc();
+    while(!canFire()){} 
+      index_disc();
     pros::delay(800);
   }
 }
@@ -71,15 +70,12 @@ void drive_for_discs(){
   drive->setMaxVelocity(0.8*x);
   drive->turnAngle(112_deg);
 
-
-
-
-
 }
 
 
 void autonomous()
 {
+<<<<<<< HEAD
     // pros::Task flywheel_on(flywheel_on_fn);
     // flywheel_speed = 102;
     // pros::delay(2000);
@@ -88,16 +84,24 @@ void autonomous()
     
 
     pros::Task flywheel_on(flyCalc, nullptr);
-    setFlyAuto(56.5);
-    pros::delay(200);
+    setFlyAuto(58);
     color_quick_spin();
     doubleAuto();
     drive_for_discs();
     setFlyAuto(53);
     pros::delay(1000);
     tripleAuto();
-
-    
+=======
+  setup_chassis;
+  Motor colorwheel(COLORWHEEL_PORT);
+  drive->moveDistance(-18_in);
+  drive->turnAngle(-60_deg);
+  drive->moveDistance(24_in);
+  drive->turnAngle(60_deg);
+  colorwheel.moveVoltage(6000);
+  drive->moveDistance(4_in);
+  colorwheel.moveVoltage(0);
+>>>>>>> 78f270792de0aa9e3f69fd95cda7f0b5bbea3ae3
 }
 
 // Helper functions
